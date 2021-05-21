8124957e87ce90e7f71ff61ac224c7fc21dc450b
mayaepps <mepps@lion.lmu.edu>
2021-05-20 19:56:57 -0700

    Added network request, tableview setup, and CocoaPod AFNetworking

---
+++ Flix/Flix/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  Flix
+//
+//  Created by Maya Epps on 5/20/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Flix/Flix/MoviesViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.h
+//  Flix
+//
+//  Created by Maya Epps on 5/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
